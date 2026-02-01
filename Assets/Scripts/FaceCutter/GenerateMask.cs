54dc73bd6f868bdc96fb7a19b3c278637313f1cc
sootstain <Sophie.Bielleman@me.com>
2026-02-01 14:25:06 +0100

    Added scaling for surgery scene

--- Assets/Scripts/FaceCutter/GenerateMask.cs
+++ Assets/Scripts/FaceCutter/GenerateMask.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class GenerateMask : MonoBehaviour
 {
     [SerializeField] SpriteRenderer spriteRenderer;
-    
     [SerializeField] BodyPartSO facePart;
     void Start()
     {
--- Assets/Scripts/FaceCutter/SOSetup/BodyPartSO.cs
+++ Assets/Scripts/FaceCutter/SOSetup/BodyPartSO.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EyeSO", menuName = "Scriptable Objects/EyeSO")]
 public class BodyPartSO : ScriptableObject
 {
-    public Vector3 position; //on mask
+    public Vector3 position = new (Random.Range(-0.5f,1.5f), Random.Range(0,1), 0); //on mask
     public Sprite sprite;
 }
